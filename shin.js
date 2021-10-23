b6107160ecfe3467c7a5fb0a6e8cb870e16edeab
Shino <90832242+Shno-rim@users.noreply.github.com>
2021-10-23 14:02:29 +0700

    Update shin.js

--- shin.js
+++ shin.js
@@ -2196,12 +2196,12 @@ query = args.join(" ")
  if (args.length == 0) return reply(`Example: ${prefix + command} shino`)
 buff = await getBuffer(`https://hadi-api.herokuapp.com/api/waifusrc?nama=${query}`)
  buttons = [{buttonId: `${prefix + command} ${query}`,buttonText:{displayText: `➡️Next`},type:1}]
-              imageMsg = (await shino.prepareMessageMedia(buff, "imageMessage", { jimp: buff, })).imageMessage
+              imageMsg = (await shino.prepareMessageMedia(buff, "imageMessage", { jimp: buff, })).imageMessage              
               buttonsMessage = {footerText:'© Shino', imageMessage: imageMsg,
               contentText:`*${query}*`,buttons,headerType:4}
               prep = await shino.prepareMessageFromContent(from,{buttonsMessage},{quoted: shin})
-              shino.relayWAMessage(prep))    
-break                
+              shino.relayWAMessage(prep)
+break  
 case 'meme':
 case 'memek':
 					if (isBanned) return reply('Maaf kamu sudah terbenned!')
